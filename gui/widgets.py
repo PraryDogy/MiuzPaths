7798e7d71d2e628385cece9915d72a1a5b2f459b
Morkik <evlosh@gmail.com>
2022-12-18 23:31:46 +0300

    refactoring

--- gui/widgets.py
+++ gui/widgets.py
@@ -1,8 +1,52 @@
-import os
+import tkinter
 
-my_path = '/Volumes/Shares/Marketing/Externarl/Фото_и_видео_Магазинов/Фото_магазинов  '
-new_path = os.path.normpath(my_path)
-while not os.path.exists(new_path):
-    new_path = os.path.split(new_path)[0]
+import cfg
 
-print(new_path)
+display = tkinter.Label
+
+
+class CBtn(tkinter.Label):
+    def __init__(self, master: tkinter, **kwargs):
+        tkinter.Label.__init__(
+            self, master, bg=cfg.BGBUTTON, fg=cfg.BGFONT, height=2)
+
+        self.bind('<Enter>', lambda e: self.enter())
+        self.bind('<Leave>', lambda e: self.out())
+
+    def press(self):
+        self['bg'] = cfg.BGPRESSED
+        cfg.ROOT.after(100, lambda: self.configure(bg=cfg.BGBUTTON))
+
+    def enter(self):
+        self['bg'] = cfg.BGSELECTED
+
+    def out(self):
+        self['bg'] = cfg.BGBUTTON
+
+    def cmd(self, cmd):
+        self.bind('<ButtonRelease-1>', cmd)
+
+
+class OpenBtn(CBtn):
+    def __init__(self, master: tkinter):
+        CBtn.__init__(self, master)
+        self['text'] = 'Открыть'
+        self.cmd(lambda e: self.open_path())
+
+    def open_path(self):
+        self.press()
+        display['text'] = 'Открытие'
+
+
+class ConvertBtn(CBtn):
+    def __init__(self, master: tkinter):
+        CBtn.__init__(self, master, text='Преобразовать')
+        self['text'] = 'Преобр.'
+
+
+class Display(tkinter.Label):
+    def __init__(self, master: tkinter):
+        tkinter.Label.__init__(self, master, bg='black', fg=cfg.BGFONT)
+        self['text'] = 'hello'
+        global display
+        display = self
