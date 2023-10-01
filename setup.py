811e90472aae112a9722266463a85920dbbf4862
evlosh <evlosh@gmail.com>
2023-10-01 17:09:59 +0300

    123

--- setup.py
+++ setup.py
@@ -7,19 +7,16 @@
 import os
 import shutil
 
-from setuptools import setup
-import cfg
 import icnsutil
+from setuptools import setup
 
+import cfg
 
 src = 'icon.png'
 img = icnsutil.IcnsFile()
 img.add_media(file=src)
 img.write(f'icon.icns')
 
-APP = ['start.py']
-
-DATA_FILES = []
 
 OPTIONS = {
     'iconfile': 'icon.icns',
@@ -35,12 +32,12 @@ OPTIONS = {
     }
 
 setup(
-    app = APP,
+    app = ['start.py'],
     name = cfg.APP_NAME,
-    data_files = DATA_FILES,
+    data_files = [],
     options = {'py2app': OPTIONS},
     setup_requires = ['py2app'],
-    install_requires = ['tkmacosx']
+    install_requires = []
     )
 
 ver = "3.11"
