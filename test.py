7d24aeb0f3ee0075becb0c63f0eda33965c5b273
Evgeny Loshkarev <evlosh@gmail.com>
2023-10-02 15:08:26 +0300

    123

--- test.py
+++ test.py
@@ -1,13 +1,22 @@
-smb = "smb://192.168.10.105/Shares/Marketing/Photo/2023/MUIZ paper bg.psd"
 path = "/Users/Loshkarev/Downloads/2022_MIUZ_Premium11576_1 1x1.jpg"
-
-script = f"""
-        set thePath to POSIX file "{path}"
-        tell application "Finder" to reveal thePath
-        tell application "Finder" to activate
-        """
+path = "smb://192.168.10.105/Shares/Marketing/Photo/2023/MUIZ paper bg.psd"
 
 
-from utils import run_applescript
 
-run_applescript(script)
+# script = f"""
+#         set thePath to POSIX file "{path}"
+#         tell application "Finder" to reveal thePath
+#         tell application "Finder" to activate
+#         """
+
+
+# from utils import run_applescript
+
+# run_applescript(script)
+
+import re
+
+reg = r"smb://.{,100}/.{,100}"
+a = re.findall(reg, path)
+
+print(a)
