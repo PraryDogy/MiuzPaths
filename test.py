e0b27edf4d70e1882292d9f8018656f156978a94
Evgeny Loshkarev <evlosh@gmail.com>
2024-01-24 12:18:42 +0300

    123

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
-"\192.168.10.105\Shares\Marketing\External\Фото_и_видео_Магазинов"
-"\sbc01\Shares\Marketing\General\4. MONTHLY TO START\МАРТ\ADV& PR\ФОТО"
-"\Marketing\Photo\2022\05 - май\Миллениум_предметка\R01-MLN119-070-_-2022-05-17 22-09-08"
-"\192.168.10.105\Shares\Google Drive\Loshkarev\Book"
-"Z:\Marketing\Photo\2022\05 - май\Миллениум_предметка\R01-MLN119-070-_-2022-05-17 22-09-08"
-"smb:/192.168.10.105/Shares/Marketing/Photo/2022/05 - май/Срочные браслеты Шмаи/Retouch"
-"/Volumes/Shares/Google Drive/Loshkarev/Book"
-"/Users/evlosh/Downloads/AutopanoGiga3.5.dmg"
-"marketing\Photo\2022\05 - май\Миллениум_предметка\R01-MLN119-070-_-2022-05-17 22-09-08"
-"\sbc01\shares\marketing\General\4. MONTHLY TO START\МАРТ\ADV& PR\ФОТО"
-"/Marketing/Photo/2022/05 - май/Срочные браслеты Шмаи/Retouch"
+import tkinter
+from tkinter import Menu, Event
+
+class ContextMenu(Menu):
+    def __init__(self, master: tkinter):
+        Menu.__init__(master=master)
+
+        self.add_command(label="Очистить", command=self.clear)
+        self.add_separator()
+        self.add_command(label="Очистить все", command=self.clear_all)
+
+    def clear(self, e: Event = None):
+        "clear current path"
+
+    def clear_all(self, e: Event = None):
+        "clear all paths"
+
+
