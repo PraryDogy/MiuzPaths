a4b0ef03ef02ff9e93adaa7c30dcf1f692409fb9
Evgeny Loshkarev <evlosh@gmail.com>
2024-01-24 13:33:05 +0300

    new set

--- setup.py
+++ setup.py
@@ -1,13 +1,7 @@
-# -*- coding: utf-8 -*-
-
-"""
-    python setup.py py2app
-"""
-
 import sys
-
 import icnsutil
 from setuptools import setup
+from datetime import datetime
 
 from setup_ext import SetupExt
 from cfg import cnf
@@ -17,30 +11,30 @@ img = icnsutil.IcnsFile()
 img.add_media(file=src)
 img.write(f'icon.icns')
 
+current_year = datetime.now().year
 
 OPTIONS = {
     'iconfile': 'icon.icns',
     'plist': {
-    'CFBundleName': cnf.app_name,
-    'CFBundleShortVersionString':cnf.app_ver,
-    'CFBundleVersion': cnf.app_ver,
-    'CFBundleIdentifier':f'com.evlosh.{cnf.app_name}',
-    'NSHumanReadableCopyright': (
-        'Created by Evgeny Loshkarev'
-        '\nCopyright © 2023 MIUZ Diamonds. All rights reserved.')
+        'CFBundleName': cnf.app_name,
+        'CFBundleShortVersionString': cnf.app_ver,
+        'CFBundleVersion': cnf.app_ver,
+        'CFBundleIdentifier': f'com.evlosh.{cnf.app_name}',
+        'NSHumanReadableCopyright': (
+            f'Created by Evgeny Loshkarev'
+            f'\nCopyright © {current_year} {cnf.app_name}. All rights reserved.')
     }
-    }
-
+}
 
 if __name__ == "__main__":
     sys.argv.append("py2app")
 
     setup(
-        app = ['start.py'],
-        name = cnf.app_name,
-        data_files = [],
-        options = {'py2app': OPTIONS},
-        setup_requires = ['py2app'],
-        )
+        app=['start.py'],
+        name=cnf.app_name,
+        data_files=[],
+        options={'py2app': OPTIONS},
+        setup_requires=['py2app'],
+    )
 
-    SetupExt(py_ver="3.11", appname=cnf.app_name)
+    SetupExt(py_ver="3.12", appname=cnf.app_name)
