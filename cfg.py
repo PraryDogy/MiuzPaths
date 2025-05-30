a5d2ffd18ee9aecd3715a22b79fed8125bc3a0ea
Evgeny Loshkarev <evlosh@gmail.com>
2025-05-30 13:16:55 +0300

    куа

--- cfg.py
+++ cfg.py
@@ -4,8 +4,6 @@ import tkinter
 class Config:
     def __init__(self):
         super().__init__()
-        self.root = tkinter.Tk()
-        self.root.withdraw()
         self.app_name = 'Paths'
         self.app_ver = '2.1.0'
 
