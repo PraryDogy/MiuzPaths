3b7af840960451aba1bba9db2b6aa97aaa461079
Morkik <evlosh@gmail.com>
2025-06-01 14:02:50 +0300

    ref

--- start.py
+++ start.py
@@ -1,11 +1,17 @@
 import tkinter
 
+import customtkinter
+
 from cfg import Cfg
 from utils import MainItem
 from widgets.main_win import MainWin
 
 root = tkinter.Tk()
 root.withdraw()
+
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("themes/metal.json")
+
 main_item = MainItem()
 cfg = Cfg()
 app = MainWin(root, main_item, cfg)
